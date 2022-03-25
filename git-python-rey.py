03b8ee39b9e9c712325f05c9af2eef88e8a21ced
ReyAlyannahMarie <reyalyannah10@gmail.com>
2022-03-25 14:52:44 +0800

    My Educational Background added

---
+++ git-python-rey.py
@@ -0,0 +1,6 @@
+print ('Alyannah Marie Rey');
+print ('Hupi Santa Cruz Marinduque');
+print ('October 10, 2001');
+print ('Marinduque State College');
+print ('Bachelor of Science in Information Technology');
+print ('2-F')
